403aa61f893101b4fe73169cf001c129111c597a
Phillip Bailey <ztron500@gmail.com>
2024-03-20 17:33:01 -0400

    Update goback.jsx

--- app/components/goback.jsx
+++ app/components/goback.jsx
@@ -4,11 +4,18 @@ import { useRouter } from "next/navigation"
 
 export default function Goback() {
     const router = useRouter()
+    
+      const handleGoBack = () => {
+    // Navigate back to the previous page
+    router.back()
+
+window.scrollTo(0, 0)
+  }
   return (
     <>
-    <button onClick={() => router.back()} className="backbtn">
-&#8592; go back
-</button>
+      <button onClick={handleGoBack} className="backbtn">
+        &#8592; go back
+      </button>
     </>
   )
 }
